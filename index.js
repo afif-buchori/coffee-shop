8f3ce8a6f751d0ca837e0bf0ed8fd5f7a78ee1cc
afif buchori <4fifbuchori@gmail.com>
2023-03-02 19:36:34 +0700

    tes endpoint

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ import express from "express";
 
 const app = express();
 const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Welcome Coffee Shop",
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`server is running at port: ${PORT}`);
     console.log('welcome');
