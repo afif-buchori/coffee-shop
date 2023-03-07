0c9969145ddc4d0fc10ddd8590fdaff6ed652889
afif buchori <4fifbuchori@gmail.com>
2023-03-07 09:16:11 +0700

    fix serverport

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const PORT = 8080;
+
+const { serverPort } =require("./src/configs/environment")
+const PORT = serverPort || 8080;
 
 //parser for body
 app.use(express.urlencoded({ extended: false })); //form-url_encode (if obj in obj 'true')
@@ -12,7 +14,6 @@ app.use(express.json()); //raw json
 const masterRouter = require("./src/routers");
 app.use(masterRouter);
 
-app.use('/images', express.static('E:/PICTURE/IMG_0385.JPG'));
 
 app.listen(PORT, () => {
     console.log(`server is running at port: ${PORT}`);
